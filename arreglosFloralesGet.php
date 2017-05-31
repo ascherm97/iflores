f93e64b5b56b0c62d297847d21b49f83592e101f
Michel Valencia <ascherm97@gmail.com>
2017-05-31 03:13:41 -0500

    Fixed /ArreglosFloralesGet.php endpoint, now the JSON is well formatted

--- arreglosFloralesGet.php
+++ arreglosFloralesGet.php
@@ -11,10 +11,21 @@
          include 'iflores.inc';
          //DB connection setup
          include 'dbConn.inc';
+         // $conexion = mysqli_connect("mysql.hostinger.mx","u542056880_root", "password.iflores.MVV","u542056880_iflor");
+         // mysqli_query($conexion, "SET NAMES 'utf8'");
+         if (!$conexion) {
+             echo "Error: Unable to connect to MySQL." . PHP_EOL;
+             exit;
+         }
 
-         $sql = "SELECT * FROM arreglofloral JOIN paquete ON idArreglo=fkArreglo";
+
+         $sql = "SELECT * FROM arreglofloral LEFT JOIN paquete ON idArreglo=fkArreglo";
          $res = mysqli_query($conexion, $sql);
-         echo json_encode(mysqli_fetch_array($res, MYSQLI_BOTH));
+         $rows = array();
+         while($r = mysqli_fetch_assoc($res)) {
+             $rows[] = $r;
+         }
+         echo json_encode($rows);
          // $direccion = new Direccion('Circunvalación Norte','Satelite','32','6',
          //                            'Napoles','66723','Zaguan negro, con jardinera al frente.');
          // $cliente = new Cliente('Jose Montes Hernández','5512345678','someone@example.com', $direccion);
