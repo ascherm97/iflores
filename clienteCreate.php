c7fac161a0ae731155a07af115b1b9f92cd235c8
Michel Jordan Valencia Rangel <ascherm97@gmail.com>
2017-06-04 03:56:09 -0500

    Added clienteCreate endpoint, added guardar methods to class Cliente and Direccion

---
+++ clienteCreate.php
@@ -0,0 +1,39 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+$data = json_decode(file_get_contents('php://input'), true);
+include 'Cliente.inc';
+
+//Peticion vacia
+if (is_null($data)) {
+    echo json_encode(array('errorCode' => 400 ));
+    return;
+}
+
+//Inicializar objeto direccion con las variables recibidas
+$direccion= new Direccion(
+    $data["direccion"]["calle"],
+    $data["direccion"]["colonia"],
+    $data["direccion"]["numeroExterior"],
+    $data["direccion"]["numeroInterior"],
+    $data["direccion"]["ciudad"],
+    $data["direccion"]["codigoPostal"],
+    $data["direccion"]["referencia"]
+);
+
+//Inicializar objeto cliente con las variables recibidas
+$cliente= new Cliente(
+    $data["nombre"],
+    $data["telefono"],
+    $data["email"],
+    $data["password"],
+    $direccion
+);
+
+//Intentar guardar el objeto en la BD
+if (!$cliente->guardar()){
+    echo json_encode(array('code' => 500 ));
+}
+
+//Como se completo el registro regresar el id del clietne
+echo json_encode(array('idArreglo' => $cliente->idCliente));
+?>
