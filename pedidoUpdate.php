32347f8124ceee95ec39d0c6d4099eb75c123213
ascherm97@gmail.com <ascherm97@gmail.com>
2017-06-06 02:19:41 -0500

    Added pedidoUpdate endpoint to change the estatus of one pedido

---
+++ pedidoUpdate.php
@@ -0,0 +1,49 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+include 'Pedido.inc';
+//Obtener todos los datos del JSON de entrada
+$data = json_decode(file_get_contents('php://input'), true);
+//ID de pedido invalido
+if (is_null($data) or !isset($data["idPedido"])
+    or !is_int($data["idPedido"])) {
+    echo json_encode(array('errorCode' => 400 ));
+    return;
+}
+//Validar estatus sea int
+if (!is_int($data["estatus"])) {
+    echo json_encode(array('errorCode' => 400 ));
+    return;
+}
+//Validar estatus tenga un valor 0,1,2
+if ($data["estatus"]<0 or $data["estatus"]>2) {
+    echo json_encode(array('errorCode' => 400 ));
+    return;
+}
+
+//DB connection setup
+include 'dbConn.inc';
+//Checar si hay conexion con la base de datos
+if (!$conexion) {
+    echo json_encode(array('code' => 500 ));
+    exit;
+}
+
+//Obtener la inforamcion de un producto
+$sql = "UPDATE pedido SET estatus='".$data["estatus"]."' WHERE idPedido= '".$data["idPedido"]."'";
+$res = mysqli_query($conexion, $sql);
+//Checar si dio resultado
+if (!$res){
+    echo json_encode(array('errorCode' => 400 ));
+    exit;
+}
+$filasAfectadas = mysqli_affected_rows($conexion);
+//Si no se creo el pedido, salir
+if ($filasAfectadas != 1) {
+    echo json_encode(array('errorCode' => 400 ));
+    exit;
+}
+mysqli_close($conexion);
+
+//Como se completo el registro regresar el id del arreglo
+echo json_encode(array('code' => $data["idPedido"]));
+?>
