85002d16e8ea8de67974a5e333f3fce698cde34e
ascherm97@gmail.com <ascherm97@gmail.com>
2017-06-07 04:12:29 -0500

    Now creates a paquete based on an arreglo

--- paqueteCreate.php
+++ paqueteCreate.php
@@ -7,23 +7,18 @@ if (is_null($data)) {
     echo json_encode(array('errorCode' => 400));
     return;
 }
-// Inicializar objeto con las variables recibidas
-$paquete = new Paquete($data["nombre"], $data["descripcion"], $data["precio"],
-    $data["disponibilidad"], $data["contenidoExtra"]);
+//Validar idArreglo sea int
+if (!is_int($data["idArreglo"])) {
+    echo json_encode(array('errorCode' => 400 ));
+    return;
+}
+/// Inicializar objeto con el idArreglo y contenido extra
+$paquete = new Paquete($data["idArreglo"], $data["contenidoExtra"]);
 // Intentar guardar el objeto en la BD
 if (!$paquete->guardar()){
     echo json_encode(array('code' => 500 ));
     exit;
 }
- //Agregar cada url a la BD
-//$imagenesAgregadas = 0;
-//foreach ($data["imagenes"] as $imagen) {
-    //if ($paquete->agregarImagen($imagen)){
-        //$imagenesAgregadas = $imagenesAgregadas + 1;
-    //}
-//}
-//unset($imagen);
-
 // Como se completo el registro regresar el id del arreglo
 echo json_encode(array('idPaquete' => $paquete->idPaquete));
 ?>
