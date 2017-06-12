57baa3fe4fed3b8025384068ee2e3b9bb9d05900
ascherm97@gmail.com <ascherm97@gmail.com>
2017-06-12 03:21:09 -0500

    Added endpoint for getting all of the pedidos

---
+++ pedidosGet.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+//Class definitions
+include 'Pedido.inc';
+//DB connection setup
+include 'dbConn.inc';
+//Checar si hay conexion con la base de datos
+if (!$conexion) {
+    echo json_encode(array('code' => 500 ));
+    exit;
+}
+
+//Obtener la inforamcion de un producto
+$sql = "SELECT * FROM resumenPedido";
+$res = mysqli_query($conexion, $sql);
+//Checar si dio resultado
+if (!$res){
+    echo json_encode(array('errorCode' => 400 ));
+    exit;
+}
+//Serializar la informacion para regresarla como un JSON
+$filas = array();
+while($r = mysqli_fetch_assoc($res)) {
+    $filas[] = $r;
+}
+echo json_encode(array('pedidos' => $filas));
+?>
